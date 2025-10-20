4adf79ea53013701dd037874dcb58fe702fb1a47
germanoj <jerradgermano@gmail.com>
2025-10-20 11:37:11 -0600

    Completed Secure the Vault assignment

---
+++ index.js
@@ -0,0 +1,7 @@
+let code1;
+code1 = 7 + 3;
+let code2 = 82 / 2 - 1;
+let code3 = (3 % 2) + 38;
+let message = "The vault has been secured. The combination is: ";
+let codeA = code1 + "-" + code2 + "-" + code3;
+let codeB = `${code1}-${code2}-${code3}`;
